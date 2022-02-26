d556a788b2a981051a5f97fa50f53999d4ada682
hristo-mavrodiev <h.mavrodiev@abv.bg>
2022-02-26 21:58:46 +0200

    switch to https links in faq

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -17,7 +17,7 @@ const FAQ: React.FC = () => (
       />
       <Container>
         <ContentBox
-          image='http://assets.iotabots.io/compressed/11.png'
+          image='https://assets.iotabots.io/compressed/11.png'
           // eslint-disable-next-line max-len
           text='An IOTABOT is a NFT in the form of a profile picture (PFP) that gives you access to the IOTABOTVERSE. At this time they are NFTs (ERC721 Tokens) on the public IOTA Smart Contract EVM Testnet.'
           headline='WHAT IS AN IOTABOT?!'
@@ -25,7 +25,7 @@ const FAQ: React.FC = () => (
           { }
         </ContentBox>
         <ContentBox
-          image='http://assets.iotabots.io/compressed/987.png'
+          image='https://assets.iotabots.io/compressed/987.png'
           // eslint-disable-next-line max-len
           text='1048 - The first 1000 IOTABOTS could be minted for free - the other was randomly choosen in Givaways mostly on Twitter.'
           headline='How many NFTs exist?'
@@ -33,7 +33,7 @@ const FAQ: React.FC = () => (
           { }
         </ContentBox>
         <ContentBox
-          image='http://assets.iotabots.io/compressed/990.png'
+          image='https://assets.iotabots.io/compressed/990.png'
           // eslint-disable-next-line max-len
           text='10.000 unique IOTABOTS.'
           headline='How many NFTs will exist?'
@@ -41,7 +41,7 @@ const FAQ: React.FC = () => (
           { }
         </ContentBox>
         <ContentBox
-          image='http://assets.iotabots.io/compressed/187.png'
+          image='https://assets.iotabots.io/compressed/187.png'
           // eslint-disable-next-line max-len
           text='Current IOTABOTS are deployed on the tesnet. The SaveTheBots initiative tries to make a successfull transition to the mainnet - so the users can hold their IOTABOTS.'
           headline='What does SaveTheBots mean?'
@@ -49,7 +49,7 @@ const FAQ: React.FC = () => (
           { }
         </ContentBox>
         <ContentBox
-          image='http://assets.iotabots.io/compressed/845.png'
+          image='https://assets.iotabots.io/compressed/845.png'
           // eslint-disable-next-line max-len
           text='Download your favourite one and use it as a profile picture.'
           headline='What can I do with my IOTABOT?'
@@ -57,7 +57,7 @@ const FAQ: React.FC = () => (
           { }
         </ContentBox>
         <ContentBox
-          image='http://assets.iotabots.io/compressed/895.png'
+          image='https://assets.iotabots.io/compressed/895.png'
           // eslint-disable-next-line max-len
           text='You need Metamask with the following custom network settings: RPC URL = https://evm.wasp.sc.iota.org and Chain ID = 1074'
           headline='How can i connect to the public IOTA EVM testnet?'
